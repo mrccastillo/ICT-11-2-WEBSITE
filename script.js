37551f0b5a974e6e5356c7fa1bf9f98e71ca349e
mrccastillo <124692706+mrccastillo@users.noreply.github.com>
2023-03-24 02:39:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function checkAuthState() {
     } else {
       //When deployed to github
       window.location.href = "/ICT-11-2-WEBSITE/index.html";
-//       window.location.href = "/index.html";
+      window.location.href = "/index.html";
     }
   });
 }
